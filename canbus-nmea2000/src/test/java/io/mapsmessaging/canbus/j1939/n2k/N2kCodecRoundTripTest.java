3d13c7d958e3e7dda99e27dc91af0582dc9b8f9c
Matthew Buckton <matthew@buckton.org>
2026-05-28 10:37:54 +1000

    test(n2k): Add test for decoding unknown PGN to JSON without registered message

--- canbus-nmea2000/src/test/java/io/mapsmessaging/canbus/j1939/n2k/N2kCodecRoundTripTest.java
+++ canbus-nmea2000/src/test/java/io/mapsmessaging/canbus/j1939/n2k/N2kCodecRoundTripTest.java
@@ -35,6 +35,27 @@ class N2kCodecRoundTripTest {
   }
 
   @Test
+  void unknownPgn_decodesToJsonWithoutRegisteredMessage() throws Exception {
+    N2kCompiledRegistry registry = buildRegistry();
+    N2kMessageParser parser = new N2kMessageParser(registry);
+
+    int unknownPgn = 130999;
+    byte[] payload = new byte[] {
+        0x01,
+        0x02,
+        0x03,
+        0x04,
+        0x05,
+        0x06,
+        0x07,
+        0x08
+    };
+
+    JsonObject decodedBack = parser.decodeToJson(unknownPgn, payload);
+    assertNull(decodedBack);
+  }
+
+  @Test
   void pgn126998_configurationInformation_roundTrip() throws Exception {
     N2kCompiledRegistry registry = buildRegistry();
     N2kMessageParser parser = new N2kMessageParser(registry);
